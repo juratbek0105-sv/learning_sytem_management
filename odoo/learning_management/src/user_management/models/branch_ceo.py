0a0934db7cc0119a2ef144da3a243299bf1ab1e2
juratbek0105-svg <juratbek0105@gmail.com>
2025-10-28 20:10:01 +0500

    lms

--- odoo/learning_management/src/user_management/models/branch_ceo.py
+++ odoo/learning_management/src/user_management/models/branch_ceo.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 
 
 class Student(models.Model):
-    _name = "lms.branch.boss"
+    _name = "lms.branch.ceo"
     _inherits = {"res.users": 'user_id'}
 
     user_id = fields.Many2one("res.users")
